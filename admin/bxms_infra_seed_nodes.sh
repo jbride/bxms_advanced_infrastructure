2484ac7edb13b35e1e9cd666837f8ee08ef92b2f
jbride <jbride2001@yahoo.com>
2016-12-07 04:19:58 -0700

    https://github.com/redhat-gpe/bxms_advanced_infrastructure/issues/18

---
+++ admin/bxms_infra_seed_nodes.sh
@@ -0,0 +1,12 @@
+# Gogs
+docker pull openshiftdemos/gogs:latest
+#docker pull registry.access.redhat.com/rhscl/postgresql-94-rhel7:latest
+
+# Nexus
+docker pull openshiftdemos/nexus:2.13.0-01
+
+# Jenkins
+#docker pull registry.access.redhat.com/openshift3/jenkins-1-rhel7:latest
+
+# Decision Server
+# docker pull registry.access.redhat.com/jboss-decisionserver-6/decisionserver63-openshift:latest
